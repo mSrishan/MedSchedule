15239b13bc1cace705d5fd5efcdd03d28577869c
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-10-08 12:28:19 +0530

    first commit

---
+++ src/main/java/com/srishan/MedSchedule/MedScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.srishan.MedSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srishan/MedSchedule/MedScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srishan.MedSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
